1837cf80495c9d0c8ace1628b5204a721c215c9b
NikS8 <nik.8@i.ua>
2018-06-06 22:27:34 +0300

    1 и 3 по одной формуле

--- a7f346.ino
+++ a7f346.ino
@@ -18,9 +18,9 @@ void loop() {
     Serial.print("   x3= ");
     Serial.print(x3);
     */
-    float ac1 = 2.4E-08*x1*x1*x1+6e-06*x1*x1+0.001*x1+0.08;
-    float ac2 = 2.4E-08*x2*x2*x2+6e-06*x2*x2+0.001*x2+0.08;
-    float ac3 = 2.4E-08*x3*x3*x3+6e-06*x3*x3+0.001*x3+0.08;
+    float ac1 = 2.8e-08*x1*x1*x1+7.4e-06*x1*x1+0.00057*x1+0.1;
+    float ac2 = 2.2e-08*x2*x2*x2+5e-06*x2*x2+0.0014*x2+0.06;
+    float ac3 = 2.8e-08*x1*x1*x1+7.4e-06*x1*x1+0.00057*x1+0.1;
     Serial.print("   ");
     Serial.print(ac1);
     Serial.print("   ");
